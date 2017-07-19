3a9480083c758209021d081b96e6acd09db07d78
z0795 <zhanghn0795@dtdream.com>
2017-07-19 09:58:41 +0800

    Initial Commit

---
+++ ZHNCountDownView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHNCountDownView
+//
+//  Created by zhn on 2017/7/19.
+//  Copyright © 2017年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
